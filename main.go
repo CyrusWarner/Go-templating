c118076341265658c8ff3a8c97c0fa4ae5be6f85
CyrusWarner <cyruswarner27@gmail.com>
2021-12-13 11:26:54 -0600

    Added method as an example for creating templates using the os package and io copy.

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	template()
+	// CreateTemplateWithDumpingToIndexFile()
+	// CreateTemplateByCreatingAFileUsingTheOsPackage
 }
 
 // running go main.go > index.html will dump the template html into an index file
-func template() {
+func CreateTemplateWithDumpingToIndexFile() {
 	name := "Cyrus Warner"
 	template := `
 	<!DOCTYPE html>
@@ -23,5 +30,28 @@ func template() {
 	`
 
 	fmt.Println(template)
+}
 
+func CreateTemplateByCreatingAFileUsingTheOsPackage() {
+	name := "Cyrus Warner"
+	str := fmt.Sprint(`
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="TUF-8>
+	<title>Hello World!</title>
+	</head>
+	<body>
+	<h1>` + name + `</h1>
+	</body>
+	</html>
+	`)
+
+	nf, err := os.Create("index.html") // Creates a file using the standard os library
+	if err != nil {
+		log.Fatal("Error creating file", err)
+	}
+	defer nf.Close() // we defer the nf.Close function to run after the template method is complete
+
+	io.Copy(nf, strings.NewReader(str))
 }
